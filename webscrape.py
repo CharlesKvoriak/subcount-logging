7af9d50f5cf95036c701d7d911d7b4a5ed4026f1
smesoftwarecharlie <charliemckennakvoriak@gmail.com>
2018-01-27 12:04:54 -0800

    added function to get the subcount of any youtuber from socialblade. Also began work on function to log subcount to a csv file

--- webscrape.py
+++ webscrape.py
@@ -1,6 +1,23 @@
 from bs4 import BeautifulSoup
 import requests
+import os.path
 
-r = requests.get("https://socialblade.com/youtube/user/carykh/realtime")
+def getsubcount(user):
+	r = requests.get("https://socialblade.com/youtube/user/"+user+"/realtime")
 
-soup = BeautifulSoup(r, 'html.parser')
+	html = r.text
+
+	soup = BeautifulSoup(html, 'html.parser')
+
+	countline = soup.find(id="rawCount")
+
+	count = str(countline.string)
+
+	return count
+
+def log(user):
+	writepath = "log.csv"
+	mode = 'a' if os.path.exists(writepath) else 'w'
+
+	with open(writepath, mode) as f:
+    	f.write('subscriber data for'+user+'\n')
